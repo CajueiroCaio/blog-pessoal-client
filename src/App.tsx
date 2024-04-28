492768e461acb22a0ec9c8ac8868868f142560ec
CajueiroCaio <caiovcajueiro@gmail.com>
2024-04-28 16:40:17 -0300

    sistema de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
-import Home from './paginas/home/Home';
+import React from 'react';
 import './App.css';
-import React, { useState } from 'react';
-
+import Home from './paginas/home/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './paginas/login/Login';
 function App() {
-  const [valor, setValor] = useState(0);
-
-  function handleClick() {
-    setValor(valor + 1);
-  }
   return (
-    //  <Home  title="Componente Home"
-    //         description="Este é um componente Home que recebe props."/>
-    <div>
-      <p>O valor é: {valor}</p>
-      <button onClick={handleClick}>Adicionar 1</button>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
-
 export default App;
