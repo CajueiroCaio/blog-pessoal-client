f4e178819a6e67bb7181371fd79aa748a7af30c2
CajueiroCaio <caiovcajueiro@gmail.com>
2024-04-29 01:39:40 -0300

    Perfil

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,22 +1,31 @@
-import React from 'react'
 import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../contexts/AuthContext'
 
 function Footer() {
- 
-  
 
-  return (
-    <>
+  const { usuario, handleLogout } = useContext(AuthContext)
+
+  let footerComponent
+
+  let data = new Date().getFullYear()
+
+  if(usuario.token !== '') {
+    footerComponent = (
+      <>
         <div className="flex justify-center bg-purple-900 text-white">
           <div className="container flex flex-col items-center py-4">
-            <p className='text-xl font-bold'>Blog Pessoal | Copyright: 2024 </p>
-            <p className='text-lg'>Acesse nossas redes sociais</p>
-            <div className='flex gap-2'>
-              <LinkedinLogo size={48} weight='bold' />
-            </div>
+            <p className='text-xl font-bold'>Blog Pessoal | Copyright: {data}</p>
           </div>
         </div>
       </>
+    )
+  }
+
+  return (
+    <>
+      {footerComponent}
+    </>
   )
 }
 
